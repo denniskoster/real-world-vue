7c8ef484cf3660ab3cd96fd8f192571d457e16ee
Dennis Koster <dennis@myparcel.com>
2018-09-24 09:58:13 +0200

    :sparkles: Finish real world vue workshop

---
+++ src/views/EventShow.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Showing event {{ id }}</h1>
+        <EventCard :event="event"/>
+    </div>
+
+</template>
+
+<script>
+  import EventService from '@/services/EventService.js'
+  import EventCard from '@/components/EventCard'
+export default {
+  props: ['id'],
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      event: {}
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data
+      }).catch((error) => {
+        console.log('There  was an error: ' + error)
+      })
+  }
+};
+</script>
